aefb32d3beb331e759b97b7e696cf53e48d993fb
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-09-23 15:36:14 +0300

    crops model

--- django_test/seedinsight/crops/models.py
+++ django_test/seedinsight/crops/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Seed(models.Model):
+    crop = models.CharField(max_length=100)
+    variety = models.CharField(max_length=100)
+    altitude_zone = models.CharField(max_length=100)
+    maturity = models.CharField(max_length=50)
+    rate = models.CharField(max_length=50)
+    yield_per_acre = models.CharField(max_length=50)
+    attributes = models.TextField()
+
+    def __str__(self):
+        return f"{self.crop} ({self.variety})"
+
