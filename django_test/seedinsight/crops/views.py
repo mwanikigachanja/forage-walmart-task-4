13c5d33e8d3a5e519f2d125af0d294a396fe13ed
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-09-24 11:05:37 +0300

    Materially abled

--- django_test/seedinsight/crops/views.py
+++ django_test/seedinsight/crops/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Seed
 
 # Create your views here.
+def get_seed_recommendations(request):
+    altitude_zone = request.GET.get('altitude_zone', 'Unknown Altitude')
+    seeds = Seed.objects.filter(altitude_zone=altitude_zone)
+    return render(request, 'seed_recommendations.html', {'seeds': seeds, 'altitude_zone': altitude_zone})
