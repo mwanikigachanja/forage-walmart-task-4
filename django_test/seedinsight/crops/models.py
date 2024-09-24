a2d8dc3d2bd8dfd068900fbeb06a7d606fbec47d
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-09-24 10:26:44 +0300

    Moderate

--- django_test/seedinsight/crops/models.py
+++ django_test/seedinsight/crops/models.py
@@ -4,7 +4,7 @@ class Seed(models.Model):
     crop = models.CharField(max_length=100)
     variety = models.CharField(max_length=100)
     altitude_zone = models.CharField(max_length=100)
-    maturity = models.CharField(max_length=50)
+    maturity = models.CharField(max_length=100)
     rate = models.CharField(max_length=50)
     yield_per_acre = models.CharField(max_length=50)
     attributes = models.TextField()
