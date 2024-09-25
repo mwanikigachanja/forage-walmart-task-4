b62502a535afd042475a264bc50e5cffd4c99b78
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-09-25 11:25:58 +0300

    Models fized

--- django_test/seedinsight/crops/models.py
+++ django_test/seedinsight/crops/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
-class Seed(models.Model):
-    crop = models.CharField(max_length=100)
+class Crop(models.Model):
+    name = models.CharField(max_length=100)
     variety = models.CharField(max_length=100)
     altitude_zone = models.CharField(max_length=100)
     maturity = models.CharField(max_length=100)
-    rate = models.CharField(max_length=50)
-    yield_per_acre = models.CharField(max_length=50)
+    rate = models.DecimalField(max_digits=5, decimal_places=2)
+    yield_amount = models.DecimalField(max_digits=5, decimal_places=2)
     attributes = models.TextField()
 
     def __str__(self):
-        return f"{self.crop} ({self.variety})"
-
+        return f"{self.name} - {self.variety}"
